4127ae125f54174996bbf93c6074f1895322bd93
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-09-28 20:50:09 +0700

    update layout home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Hero from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div>
-      Home
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Have a good idea to write about?</h1>
+        <p className={styles.desc}>
+          Write whatever ideas you have here, so that other people can see them.
+        </p>
+        <button className={styles.button}>Write Now</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="main image" className={styles.img} />
+      </div>
     </div>
   )
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
         <div>©2023 Just Write. All rights reserved.</div>
-        <div></div>
+        <div className={styles.social}>
+          <Image src="/1.png" className={styles.icon} width={15} height={15} alt="Social Media" />
+          <Image src="/2.png" className={styles.icon} width={15} height={15} alt="Social Media " />
+          <Image src="/3.png" className={styles.icon} width={15} height={15} alt="Social Media" />
+          <Image src="/4.png" className={styles.icon} width={15} height={15} alt="Social Media" />
+        </div>
     </div>
   )
 }
