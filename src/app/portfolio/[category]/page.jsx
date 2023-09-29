0931075064ab3d8955a8ebd2de97458805948fac
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-09-29 10:27:03 +0700

    update layput porto page and detail

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import styles from './page.module.css'
+import Button from '@/components/button/Button'
+import Image from 'next/image'
 
-const Category = () => {
+const Category = ({params}) => {
+  console.log(params)
   return (
-    <div>Category</div>
+    <div className={styles.container}>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.img}
+            fill={true}
+            alt=""
+            src="https://images.pexels.com/photos/3194521/pexels-photo-3194521.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
