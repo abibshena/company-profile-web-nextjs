5a9a0417bdf7cc5b06ffbe2f339e1d75208cdd3b
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-09-29 22:16:55 +0700

    add func request data on server side for blog

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,13 +1,27 @@
 import React from 'react'
 import styles from './page.module.css'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 
-const BlogPost = () => {
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { 
+    cache: 'no-store' 
+  })
+
+  if (!res.ok) {
+    return notFound()
+  }
+ 
+  return res.json()
+}
+
+const BlogPost = async ({params}) => {
+  const data = await getData(params.id)
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.info}>
-          <h1 className={styles.title}>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h1>
+          <h1 className={styles.title}>{data.title}</h1>
           <p className={styles.desc}>
           Nullam eget semper justo, sit amet lacinia arcu. Phasellus tristique metus vel justo vehicula, 
           sed euismod mi consequat. Praesent lacinia, tortor at facilisis fermentum, urna nisi bibendum elit,
