81054df5144c26d8e2c6beb536f6281840817bf2
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-10-01 23:29:11 +0700

    try change url endpoint

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 
 async function getData() {
-  const res = await fetch(`${process.env.HOST_API}/api/posts`, { 
+  // console.log('lewat sini >>>', window.location.origin)
+  const res = await fetch(`https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/posts`, { 
     cache: 'no-store' 
   })
 
