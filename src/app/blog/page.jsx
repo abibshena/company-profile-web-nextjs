2c98b2d2feac5b56f83b644f9c9f6b209e5da2ae
Lutfi Maulana <lutfimaulana@Lutfis-MacBook-Air-2.local>
2023-10-02 18:27:10 +0700

    try clear all cache for this page

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { getPosts } from '@/app/api/posts/route'
 
+export const dynamic = 'force-dynamic'
+
 async function getData() {
   const res = await getPosts()
 
